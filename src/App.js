9d5178550d5d8bfe13fec7573e2852543d10b7d2
popo63301 <popo63301@gmail.com>
2018-11-23 08:40:26 +0100

    create UI

--- src/App.js
+++ src/App.js
@@ -2,7 +2,16 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-    return <div> toto</div>;
+    return (
+      <div>
+        <div>
+          <textarea rows="30" cols="30" />
+          <textarea rows="30" cols="20" style={{ marginLeft: '200px' }} />
+          <textarea rows="30" cols="20" style={{ marginLeft: '100px' }} />
+        </div>
+        <button type="button">DIVIDE</button>
+      </div>
+    );
   }
 }
 
